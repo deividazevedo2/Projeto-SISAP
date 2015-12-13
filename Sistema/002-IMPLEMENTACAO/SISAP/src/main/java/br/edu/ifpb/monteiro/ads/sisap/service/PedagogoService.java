f21e4717ddcf2767db5103ed3959659d1f855ca3
IndyPaula <indypaula12@gmail.com>
2015-12-13 17:21:17 -0300

    Alterações na Classe pedagogoService

--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/service/PedagogoService.java
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/service/PedagogoService.java
@@ -1,5 +1,70 @@
 package br.edu.ifpb.monteiro.ads.sisap.service;
 
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+
+import br.com.ifpb.monteiro.ads.sisap.exception.SisapException;
+import br.edu.ifpb.monteiro.ads.sisap.dao.PedagogoDAO;
+import br.edu.ifpb.monteiro.ads.sisap.entities.Pedagogo;
+
 public class PedagogoService {
+
+	// @Inject
+	private PedagogoDAO pedagogoDAO;
+
+	// @TransacionalCdi
+	private void addPedagogo(Pedagogo pedagogo) throws SisapException {
+		try {
+			this.pedagogoDAO.salvar(pedagogo);
+		} catch (SisapException exception) {
+			throw new SisapException(exception.getMessage());
+		}
+
+	}
+
+	private Pedagogo editarPedagogo(Pedagogo pedagogo) throws SisapException {
+		try {
+			return pedagogoDAO.alterar(pedagogo);
+		} catch (SisapException exception) {
+			throw new SisapException(exception.getMessage());
+		}
+	}
+
+//	private void excluirPedagogo(Pedagogo pedagogo) throws SisapException {
+//		try {
+//			this.pedagogoDAO.deletar(pedagogo);
+//		} catch (SisapException exception) {
+//			throw new SisapException(exception.getMessage());
+//		}
+//	}
+
+	public Pedagogo buscaPorMatricula(String matricula) throws SisapException {
+		try {
+			return this.pedagogoDAO.buscarPorMatricula(matricula);
+		} catch (PersistenceException exception) {
+			throw new SisapException(exception.getMessage(), exception);
+		}
+
+	}
+
+	public Pedagogo getById(Long idPedagogo) throws SisapException {
+		try {
+			return this.pedagogoDAO.buscar(idPedagogo);
+		} catch (PersistenceException exception) {
+			throw new SisapException(exception.getMessage(), exception);
+		}
+
+	}
+
+	public List<Pedagogo> getAll()
+			throws SisapException {
+		try {
+			return this.pedagogoDAO.getAll();
+		} catch (PersistenceException exception) {
+			throw new SisapException(exception.getMessage(), exception);
+		}
+
+	}
 
 }
