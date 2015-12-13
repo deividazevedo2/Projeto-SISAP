72b436ba8db0d2fdf48854ee7ba0c08d7e45effc
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-13 16:26:54 -0300

    Adicionando as classes DAO e PedagogoDAO

---
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/PedagogoDAO.java
@@ -0,0 +1,42 @@
+package br.edu.ifpb.monteiro.ads.sisap.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.edu.ifpb.monteiro.ads.sisap.entities.Pedagogo;
+
+public class PedagogoDAO extends DAO {
+
+	public void salvar(Pedagogo pedagogo) {
+		EntityManager em = getEntityManager();
+		em.persist(pedagogo);
+	}
+
+	public Pedagogo buscar(Integer matriculaSuap) {
+		EntityManager em = getEntityManager();
+		Pedagogo resultado = null;
+		resultado = em.find(Pedagogo.class, matriculaSuap);
+		return resultado;
+
+	}
+
+	public Pedagogo alterar(Pedagogo pedagogo) {
+		EntityManager em = getEntityManager();
+		Pedagogo resultado = pedagogo;
+		resultado = em.merge(pedagogo);
+		return resultado;
+
+	}
+	
+	public List<Pedagogo> getAll(){
+		EntityManager em = getEntityManager();
+		List<Pedagogo> resultado = null;
+
+		String jpql = "select a from Aluno_JS a where 1=1";
+
+		return resultado;
+		
+	}
+
+}
--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/UsuarioDAO.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifpb.monteiro.ads.sisap.dao;
-
-public class UsuarioDAO {
-
-}
