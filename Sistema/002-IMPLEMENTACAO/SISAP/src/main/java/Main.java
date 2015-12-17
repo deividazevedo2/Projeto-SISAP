54bb21cc892d6cbe8976f9ea6e64398c4f7c9587
Widancassio <widancassio.ads@gmail.com>
2015-12-17 09:30:15 -0300

    Adicionei a classe Main para testar a persistência

---
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/Main.java
@@ -0,0 +1,47 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.edu.ifpb.monteiro.ads.sisap.embedded.Endereco;
+import br.edu.ifpb.monteiro.ads.sisap.entities.Grupo;
+import br.edu.ifpb.monteiro.ads.sisap.entities.Pedagogo;
+import br.edu.ifpb.monteiro.ads.sisap.entities.Sexo;
+
+public class Main {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("SISAP");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+
+		try {
+			Endereco endereco = new Endereco();
+			endereco.setBairro("Prado");
+			endereco.setCep("55200-000");
+			endereco.setCidade("Pesqueira");
+			endereco.setNumero(11);
+			endereco.setRua("Rua Santa Agueda");
+			endereco.setUf("PE");
+
+			Pedagogo pedagogo = new Pedagogo();
+			pedagogo.setMatriculaSuap("6505026526520");
+			pedagogo.setGrupo(Grupo.PEDAGOGO);
+			pedagogo.setSexo(Sexo.MASCULINO);
+			pedagogo.setPrimeiroNome("Widancassio");
+			pedagogo.setSegundoNome("Galindo");
+			pedagogo.setCpf("000020000");
+			pedagogo.setEndereco(endereco);
+			pedagogo.setSenha("pedagogo");
+			
+			em.persist(pedagogo);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			em.getTransaction().commit();
+			emf.close();
+		}
+
+		System.out.println("It is over");
+	}
+}
