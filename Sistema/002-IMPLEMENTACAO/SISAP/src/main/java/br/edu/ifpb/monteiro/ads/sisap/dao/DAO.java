0316cfcd3547e83c3a7fa1bfbcafdcf0778b80d4
IndyPaula <indypaula12@gmail.com>
2015-12-17 08:46:36 -0300

    Criação do DAO genérico.

--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/DAO.java
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/DAO.java
@@ -5,12 +5,20 @@ import java.io.Serializable;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
-public abstract class DAO implements Serializable {
+import br.edu.ifpb.monteiro.ads.sisap.exception.SisapException;
+
+public abstract class DAO<T> implements Serializable {
 
 	private static final long serialVersionUID = -315740615092329279L;
 
 	@Inject
 	private EntityManager entityManager;
+
+	private Class<T> entity;
+
+	public DAO(Class<T> entity) {
+		this.entity = entity;
+	}
 
 	protected EntityManager getEntityManager() {
 		return entityManager;
@@ -19,4 +27,30 @@ public abstract class DAO implements Serializable {
 	public void setEntityManager(EntityManager entityManager) {
 		this.entityManager = entityManager;
 	}
+
+	public Class<T> getEntity() {
+		return entity;
+	}
+
+	public void setEntity(Class<T> entity) {
+		this.entity = entity;
+	}
+
+	public void salvar(T entity) throws SisapException {
+		entityManager.persist(entity);
+	}
+
+	public T atualizar(T entity) throws SisapException {
+		entityManager.merge(entity);
+		return entity;
+	}
+
+	public void remover(T entity) throws SisapException{
+		entityManager.remove(entity);
+	}
+
+	public T consultarPorId(Long id) throws SisapException {
+		return entityManager.find(this.entity, id);
+	}
+
 }
