344f00aa4712210a5246fd37132ab3bcd9aa121e
Widancassio <widancassio.ads@gmail.com>
2015-12-20 14:17:27 -0300

    Adicionei essa classe para se comunicar com a renderização da pagina xhtml

---
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/entities/PedagogoConverter.java
@@ -0,0 +1,53 @@
+package br.edu.ifpb.monteiro.ads.sisap.entities;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.ifpb.monteiro.ads.sisap.dao.PedagogoDAO;
+import br.edu.ifpb.monteiro.ads.sisap.exception.SisapException;
+
+@Named
+@RequestScoped
+public class PedagogoConverter implements Converter {
+
+	@Inject
+	private PedagogoDAO pedagogos;
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String valor) {
+		if (valor == null || valor.trim().isEmpty()) {
+			return null;
+		}
+		String matricula = String.valueOf(valor);
+
+		try {
+			return pedagogos.buscarPorMatricula(matricula);
+		} catch (SisapException e) {
+			String msgErroStr = String
+					.format("Erro de conversão! Não foi possível realizar a conversão da string '%s' para o tipo esperado.",
+							valor);
+			FacesMessage msgErro = new FacesMessage(
+					FacesMessage.SEVERITY_ERROR, msgErroStr, msgErroStr);
+			throw new ConverterException(msgErro);
+		}
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object valor) {
+		if (valor == null) {
+			return null;
+		}
+		String cpfPedagogo = ((Pedagogo) valor).getCpf();
+
+		return (cpfPedagogo != null) ? cpfPedagogo.toString() : null;
+	}
+
+}
