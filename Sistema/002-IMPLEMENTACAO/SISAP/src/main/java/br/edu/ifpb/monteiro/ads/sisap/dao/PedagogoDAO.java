9c7a22f88c468c5b941e3288b70081e579311c22
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-26 15:21:01 -0300

    Javadoc adicionado, metodos de criptografia de senha e complexidade da classe diminuida

--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/PedagogoDAO.java
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/PedagogoDAO.java
@@ -11,6 +11,13 @@ public class PedagogoDAO extends DAO {
 
 	private static final long serialVersionUID = 4651136765722356561L;
 
+	/**
+	 * Realiza a persistencia da entidade Pedagogo passada como parametro no
+	 * banco de dados.
+	 * 
+	 * @param pedagogo
+	 * @throws SisapException
+	 */
 	public void salvar(Pedagogo pedagogo) throws SisapException {
 		EntityManager em = getEntityManager();
 		try {
@@ -20,6 +27,14 @@ public class PedagogoDAO extends DAO {
 		}
 	}
 
+	/**
+	 * Atualiza o registro no banco de dados de uma determinada entidade
+	 * Pedagogo passada como parametro.
+	 * 
+	 * @param pedagogo
+	 * @return
+	 * @throws SisapException
+	 */
 	public Pedagogo atualizar(Pedagogo pedagogo) throws SisapException {
 		EntityManager em = getEntityManager();
 		Pedagogo resultado = pedagogo;
@@ -31,6 +46,13 @@ public class PedagogoDAO extends DAO {
 		return resultado;
 	}
 
+	/**
+	 * Remove o registro de uma determinada entidade passada como parametro
+	 * neste metodo.
+	 * 
+	 * @param pedagogo
+	 * @throws SisapException
+	 */
 	public void remover(Pedagogo pedagogo) throws SisapException {
 		EntityManager em = getEntityManager();
 		try {
@@ -41,6 +63,14 @@ public class PedagogoDAO extends DAO {
 		}
 	}
 
+	/**
+	 * Realiza a busca da entidade Pedagogo atraves do ID passado como parametro
+	 * no metodo.
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SisapException
+	 */
 	public Pedagogo buscarPorId(Long id) throws SisapException {
 		EntityManager em = getEntityManager();
 		Pedagogo resultado = null;
@@ -53,6 +83,14 @@ public class PedagogoDAO extends DAO {
 		return resultado;
 	}
 
+	/**
+	 * Realiza a busca de uma determinada entidade PEdagogo atraves da matricula
+	 * do pedagogo passada como parametro.
+	 * 
+	 * @param matriculaPedagogo
+	 * @return
+	 * @throws SisapException
+	 */
 	public Pedagogo buscarPorMatricula(Long matriculaPedagogo)
 			throws SisapException {
 		EntityManager em = getEntityManager();
