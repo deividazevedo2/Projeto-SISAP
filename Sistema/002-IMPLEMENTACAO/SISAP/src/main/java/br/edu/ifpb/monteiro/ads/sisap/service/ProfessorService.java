18b5ce6100987cbb5cf3f44eb611416667c89be2
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-27 17:07:01 -0300

    javadoc adicionado, e removidas as clausulas try catch que faziam relancamento de excecoes.

--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/service/ProfessorService.java
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/service/ProfessorService.java
@@ -3,7 +3,6 @@ package br.edu.ifpb.monteiro.ads.sisap.service;
 import java.io.Serializable;
 
 import javax.inject.Inject;
-import javax.persistence.PersistenceException;
 
 import br.edu.ifpb.monteiro.ads.sisap.dao.ProfessorDAO;
 import br.edu.ifpb.monteiro.ads.sisap.entities.Professor;
@@ -17,53 +16,76 @@ public class ProfessorService implements Serializable {
 	@Inject
 	private ProfessorDAO professorDAO;
 
+	/**
+	 * Metodo para salvar um novo prfessor, chamando a classe professorDAO onde
+	 * a entidade em questao devera ser persistida no banco.
+	 * 
+	 * @param professor
+	 * @throws SisapException
+	 */
 	@TransacionalCdi
 	public void salvar(Professor professor) throws SisapException {
-		try {
-			this.professorDAO.salvar(professor);
-		} catch (SisapException exception) {
-			throw new SisapException(exception.getMessage());
-		}
+		this.professorDAO.salvar(professor);
 
 	}
 
+	/**
+	 * Este metodo atualiza um determinado professor que esta sendo passado como
+	 * parametro. Sera chamado o professorDAO que fara a busca no banco e ira
+	 * alterar os dados que foram modificados.
+	 * 
+	 * @param professor
+	 * @return
+	 * @throws SisapException
+	 */
 	@TransacionalCdi
 	public Professor atualizar(Professor professor) throws SisapException {
-		try {
-			return professorDAO.atualizar(professor);
-		} catch (SisapException exception) {
-			throw new SisapException(exception.getMessage());
-		}
+		return professorDAO.atualizar(professor);
 	}
 
+	/**
+	 * Metodo para remover um professor do cadastro de usuarios no banco. Sera
+	 * chamado o professorDAO que fara a busca deste registro e removera o
+	 * cadastro do mesmo.
+	 * 
+	 * @param professor
+	 * @throws SisapException
+	 */
 	@TransacionalCdi
 	public void remover(Professor professor) throws SisapException {
-		try {
-			this.professorDAO.remover(professor);
-		} catch (SisapException exception) {
-			throw new SisapException(exception.getMessage());
-		}
+		this.professorDAO.remover(professor);
 
 	}
 
+	/**
+	 * Metodo para realizar a busca do professor pelo identificador (ID) do
+	 * mesmo. O id deve ser passado como parametro para que a busca no banco
+	 * possa ser realizada, retornando o cadastro referente.
+	 * 
+	 * @param idProfessor
+	 * @return
+	 * @throws SisapException
+	 */
 	@TransacionalCdi
 	public Professor buscarPorId(Long idProfessor) throws SisapException {
-		try {
-			return this.professorDAO.buscarPorId(idProfessor);
-		} catch (PersistenceException exception) {
-			throw new SisapException(exception.getMessage(), exception);
-		}
+		return this.professorDAO.buscarPorId(idProfessor);
 	}
 
+	/**
+	 * Metodo que realiza a busca do professor atraves da matricula passada como
+	 * parametro no metodo.
+	 * 
+	 * @param matricula
+	 *            A matricula recebida eh do tipo String, porem eh feito um cast
+	 *            na chamada do metodo da classe professorDAO para que a busca
+	 *            seja realizada, ja que o metodo no DAO deve receber a
+	 *            matricula no tipo Long.
+	 * @return
+	 * @throws SisapException
+	 */
 	@TransacionalCdi
 	public Professor buscarPorMatricula(String matricula) throws SisapException {
-		try {
-			return ((ProfessorDAO) this.professorDAO)
-					.buscarPorMatricula(matricula);
-		} catch (PersistenceException exception) {
-			throw new SisapException(exception.getMessage(), exception);
-		}
-
+		return ((ProfessorDAO) this.professorDAO).buscarPorMatricula(matricula);
 	}
 
 }
