91f06cac1627a31eaa6564d0130695cbfa3a2f9e
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-27 16:58:06 -0300

    javadoc adicionado e clausulas try catch que estavam fazendo relancamento de excecao foram removidas.

--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/ProfessorDAO.java
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/ProfessorDAO.java
@@ -1,7 +1,6 @@
 package br.edu.ifpb.monteiro.ads.sisap.dao;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceException;
 
 import br.edu.ifpb.monteiro.ads.sisap.entities.Professor;
 import br.edu.ifpb.monteiro.ads.sisap.exception.SisapException;
@@ -10,59 +9,69 @@ public class ProfessorDAO extends DAO {
 
 	private static final long serialVersionUID = 4651136765722356561L;
 
+	/**
+	 * Metodo para salvar uma entidade Professor no banco de dados.
+	 * 
+	 * @param professor
+	 * @throws SisapException
+	 */
 	public void salvar(Professor professor) throws SisapException {
 		EntityManager em = getEntityManager();
-		try {
-			em.persist(professor);
-		} catch (PersistenceException pe) {
-			throw new SisapException("Erro ao salvar o cadastro!");
-		}
+		em.persist(professor);
 	}
 
+	/**
+	 * Metodo para realizar a alteracao e atualizacao dos dados de uma entidade
+	 * professor passada como parametro neste metodo.
+	 * 
+	 * @param professor
+	 * @return
+	 * @throws SisapException
+	 */
 	public Professor atualizar(Professor professor) throws SisapException {
 		EntityManager em = getEntityManager();
-		Professor resultado = professor;
-		try {
-			resultado = em.merge(professor);
-		} catch (PersistenceException pe) {
-			throw new SisapException("Erro ao alterar o cadastro!");
-		}
-		return resultado;
+		return em.merge(professor);
 	}
 
+	/**
+	 * Neste metodo pode-se remover o cadastro de uma entidade professor passada
+	 * como parametro cujo possui cadastro no banco de dados do sistema SISAP.
+	 * 
+	 * @param professor
+	 * @throws SisapException
+	 */
 	public void remover(Professor professor) throws SisapException {
 		EntityManager em = getEntityManager();
-		try {
-			professor = em.merge(professor);
-			em.remove(professor);
-		} catch (PersistenceException pe) {
-			throw new SisapException("Erro ao deletar o cadastro!");
-		}
+		professor = em.merge(professor);
+		em.remove(professor);
 	}
 
+	/**
+	 * Este metodo realiza uma busca de uma determinada entidade professor que
+	 * possui matricula correspondente a que esta sendo passada como parametro.
+	 * O objeto buscado eh retornado para os devidos fins que sera utilizado.
+	 * 
+	 * @param matriculaSuap
+	 * @return
+	 * @throws SisapException
+	 */
 	public Professor buscarPorMatricula(String matriculaSuap)
 			throws SisapException {
 		EntityManager em = getEntityManager();
-		Professor resultado = null;
-		try {
-			resultado = em.find(Professor.class, matriculaSuap);
-		} catch (PersistenceException pe) {
-			throw new SisapException(
-					"Ocorreu um problema ao buscar o cadastro!");
-		}
-		return resultado;
+		return em.find(Professor.class, matriculaSuap);
 	}
 
+	/**
+	 * Este metodo realiza uma busca da entidade professor atraves do
+	 * identificados (ID) passado como parametro.
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SisapException
+	 */
 	public Professor buscarPorId(Long id) throws SisapException {
 		EntityManager em = getEntityManager();
-		Professor resultado = null;
-		try {
-			resultado = em.find(Professor.class, id);
-		} catch (PersistenceException pe) {
-			throw new SisapException(
-					"Ocorreu um problema ao buscar o cadastro!");
-		}
-		return resultado;
+		return em.find(Professor.class, id);
 	}
 
 }
