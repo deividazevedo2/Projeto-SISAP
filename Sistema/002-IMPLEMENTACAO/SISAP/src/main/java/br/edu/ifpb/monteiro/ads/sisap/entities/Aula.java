d6a4092e2ae1c4bb6c3c7a9a22363ebfeff8aa22
Deivid Azevedo <deividazevedo1@gmail.com>
2016-01-21 21:21:21 -0300

    Inicializei o ArrayList de alunos no construtor

--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/entities/Aula.java
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/entities/Aula.java
@@ -15,8 +15,8 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "TB_DISCIPLINA")
 @DiscriminatorValue("DISCIPLINA")
-public class Aula implements Serializable{
-	
+public class Aula implements Serializable {
+
 	/**
 	 * 
 	 */
@@ -29,57 +29,47 @@ public class Aula implements Serializable{
 
 	@Column(name = "DATA")
 	private Date data;
-	
+
 	@Column(name = "CONTEUDO")
 	private String conteudo;
-	
+
 	@Column(name = "ALUNOS")
 	private ArrayList<Aluno> alunos;
-	
-	
-	public Aula() {
-		// TODO Auto-generated constructor stub
-	}
 
+	public Aula() {
+		alunos = new ArrayList<Aluno>();
+	}
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public Date getData() {
 		return data;
 	}
 
-
 	public void setData(Date data) {
 		this.data = data;
 	}
-
 
 	public String getConteudo() {
 		return conteudo;
 	}
 
-
 	public void setConteudo(String conteudo) {
 		this.conteudo = conteudo;
 	}
-
 
 	public ArrayList<Aluno> getAlunos() {
 		return alunos;
 	}
 
-
 	public void setAlunos(ArrayList<Aluno> alunos) {
 		this.alunos = alunos;
 	}
-	
-	
+
 }
