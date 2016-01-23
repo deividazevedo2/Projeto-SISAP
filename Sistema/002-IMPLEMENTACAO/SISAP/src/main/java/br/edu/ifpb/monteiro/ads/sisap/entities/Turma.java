ca217aa696ee70e74b39b10e44a7cab49a927a15
IndyPaula <indypaula12@gmail.com>
2016-01-23 14:53:26 -0300

    Classe Turma

---
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/entities/Turma.java
@@ -0,0 +1,85 @@
+package br.edu.ifpb.monteiro.ads.sisap.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_TURMA")
+@DiscriminatorValue("TURMA")
+public class Turma implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8184311990618009592L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "ALUNOS")
+	private ArrayList<Aluno> alunos;
+
+	public ArrayList<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(ArrayList<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+
+	@Column(name = "ANO_PERIODO_LETIVO")
+	private String anoPeriodoLetivo;
+
+	@Column(name = "HORARIO")
+	private String horario;
+
+	@Column(name = "SALA")
+	private String sala;
+
+	public Turma() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAnoPeriodoLetivo() {
+		return anoPeriodoLetivo;
+	}
+
+	public void setAnoPeriodoLetivo(String anoPeriodoLetivo) {
+		this.anoPeriodoLetivo = anoPeriodoLetivo;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+	public String getSala() {
+		return sala;
+	}
+
+	public void setSala(String sala) {
+		this.sala = sala;
+	}
+
+}
