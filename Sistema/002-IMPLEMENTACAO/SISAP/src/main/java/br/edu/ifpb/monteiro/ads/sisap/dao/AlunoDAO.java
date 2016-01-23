70caa2a2c67f3fcdf2e6a15c0064b9c74da7af56
IndyPaula <indypaula12@gmail.com>
2016-01-23 14:51:46 -0300

    Claasse AlunoDAO

---
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/dao/AlunoDAO.java
@@ -0,0 +1,90 @@
+package br.edu.ifpb.monteiro.ads.sisap.dao;
+
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import br.edu.ifpb.monteiro.ads.sisap.entities.Aluno;
+import br.edu.ifpb.monteiro.ads.sisap.entities.Pedagogo;
+import br.edu.ifpb.monteiro.ads.sisap.exception.SisapException;
+
+public class AlunoDAO extends DAO{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -601004838687928376L;
+	
+	private static final Log LOGGER = LogFactory.getLog(AlunoDAO.class);
+
+	
+	
+
+	/**
+	 * Realiza a busca da entidade Aluno atraves do ID passado como parametro
+	 * no metodo.
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SisapException
+	 */
+	public Aluno buscarPorId(Long id) throws SisapException {
+		EntityManager em = getEntityManager();
+		Aluno resultado = null;
+		try {
+			resultado = em.find(Aluno.class, id);
+		} catch (PersistenceException e) {
+			LOGGER.warn("Ocorreu um problema ao buscar o Aluno!", e);
+		}
+		return resultado;
+	}
+
+
+	/**
+	 * Realiza a busca de uma determinada entidade PEdagogo atraves da matricula
+	 * do pedagogo passada como parametro.
+	 * 
+	 * @param matriculaPedagogo
+	 * @return
+	 * @throws SisapException
+	 */
+	public ArrayList<Float> buscarNotaPorMatricula(Long matriculaAluno)
+			throws SisapException {
+		ArrayList<Float> notas = new ArrayList<>();
+		EntityManager em = getEntityManager();
+		Float resultado = null;
+		if (matriculaAluno == null) {
+			matriculaAluno = Long.valueOf("");
+		}
+		try {
+			TypedQuery<Pedagogo> query = em
+					.createQuery(
+							"select nota from TB_NOTA_DE_AVALIACAO where notaDaAvaliacao.aluno.matricula like :matriculaAluno",
+							Pedagogo.class);
+			query.setParameter("matricula", "%" + matriculaAluno + "%");
+			notas.add(resultado);
+		} catch (PersistenceException e) {
+			LOGGER.warn(
+					"Ocorreu um problema ao buscar as notas!",
+					e);
+		}
+		
+		return notas;
+
+	}
+
+
+	public String buscarFrequenciaDoAluno(Long matricula) {
+		return null;
+	}
+	
+	
+	
+	
+	
+}
