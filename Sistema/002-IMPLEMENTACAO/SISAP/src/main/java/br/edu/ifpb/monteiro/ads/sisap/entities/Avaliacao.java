c2084e3f656747631619d7105a645d1acec55135
IndyPaula <indypaula12@gmail.com>
2016-01-29 23:02:15 -0300

    Alteração no mapeamento

--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/entities/Avaliacao.java
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/entities/Avaliacao.java
@@ -7,9 +7,14 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity(name = "Avaliacao")
@@ -30,15 +35,23 @@ public class Avaliacao implements Serializable {
 	@Column(name = "DATA")
 	private Date data;
 
-	@Column(name = "ASSUNTO")
+//	@Column(name = "ASSUNTO")
+//	private Aula assunto;
+
+//	@Column(name = "NOTA")
+//	private List<NotaDeAvaliacao> notas;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="ASSUNTO")
 	private Aula assunto;
 
-	@Column(name = "NOTA")
+	@OneToMany(fetch= FetchType.EAGER)
+	@JoinTable(name="NOTAS_AVALICAO", joinColumns={@JoinColumn(name="AVALICAO_ID", referencedColumnName="ID")},inverseJoinColumns={@JoinColumn(name="NOTA_DE_AVALIACAO_ID", referencedColumnName="ID")})
 	private List<NotaDeAvaliacao> notas;
 
 	public Avaliacao() {
 	}
 	
-	
+	 
 
 }
