aebdbd82d2c1fe6f8b0d74788a1cf82d8c53e08b
Deivid Azevedo <deividazevedo1@gmail.com>
2016-01-30 08:42:06 -0300

    Removi o extends Pessoa para que seja criada uma nova tabela apenas para aluno. Com o extends o aluno ficaria na mesma tabela de Pessoa, onde muitos atributos de pessoa nao precisam ser de aluno.

--- Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/entities/Aluno.java
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/entities/Aluno.java
@@ -1,26 +1,83 @@
 package br.edu.ifpb.monteiro.ads.sisap.entities;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import br.edu.ifpb.monteiro.ads.sisap.embedded.Endereco;
+
+/**
+ * Classe Entidade ALUNO que servira para salvar as informacoes do aluno que
+ * virao da base de dados.
+ * 
+ * @author Deivid Azevedo
+ *
+ */
 @Entity(name = "Aluno")
 @Table(name = "TB_ALUNO")
 @DiscriminatorValue("ALUNO")
-public class Aluno extends Pessoa {
+public class Aluno implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "MATRICULA_ALUNO", unique = true)
 	private long matricula;
+
+	private String nome;
+
+	@OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.ALL }, mappedBy = "aluno")
+	private List<NotaDeAvaliacao> notas;
+
+	@Embedded
+	private Endereco endereco;
+
+	@OneToOne(fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+	@JoinColumn(name = "CONTATO_FK", nullable = false)
+	private Contato contato;
 
 	private String pai;
 
 	private String mae;
 
+	@OneToOne(fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+	@JoinColumn(name = "RESPONSAVEL_FK", nullable = false)
+	private Responsavel responsavel;
+
+	@OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.ALL }, mappedBy = "aluno")
+	private List<Atendimento> atendimentos;
+
 	public Aluno() {
+		atendimentos = new ArrayList<Atendimento>();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public long getMatricula() {
@@ -29,6 +86,30 @@ public class Aluno extends Pessoa {
 
 	public void setMatricula(long matricula) {
 		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Contato getContato() {
+		return contato;
+	}
+
+	public void setContato(Contato contato) {
+		this.contato = contato;
 	}
 
 	public String getPai() {
@@ -45,6 +126,22 @@ public class Aluno extends Pessoa {
 
 	public void setMae(String mae) {
 		this.mae = mae;
+	}
+
+	public Responsavel getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(Responsavel responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public List<Atendimento> getAtendimentos() {
+		return atendimentos;
+	}
+
+	public void setAtendimentos(List<Atendimento> atendimentos) {
+		this.atendimentos = atendimentos;
 	}
 
 }
