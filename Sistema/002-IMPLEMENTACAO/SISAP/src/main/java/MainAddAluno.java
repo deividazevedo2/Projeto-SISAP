cf4d9e5aced4745d477016f13d19b08dcb1298bd
Deivid Azevedo <deividazevedo1@gmail.com>
2016-02-12 23:25:28 -0300

    Main para adicionar alunos ao banco

---
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/MainAddAluno.java
@@ -0,0 +1,120 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.RollbackException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import br.edu.ifpb.monteiro.ads.sisap.embedded.Endereco;
+import br.edu.ifpb.monteiro.ads.sisap.entities.Aluno;
+import br.edu.ifpb.monteiro.ads.sisap.entities.Contato;
+import br.edu.ifpb.monteiro.ads.sisap.entities.Responsavel;
+
+public class MainAddAluno {
+
+	private static final Log LOGGER = LogFactory.getLog(MainAddAluno.class);
+
+	static ArrayList<Endereco> enderecos = new ArrayList<Endereco>();
+
+	static ArrayList<Contato> contatos = new ArrayList<Contato>();
+	static ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+
+	public static void adicionaAlunos() {
+
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("SISAP");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+
+		try {
+			LOGGER.info("Creating data. . . Please wait. . .");
+			retornaAluno();
+			for (Aluno aluno : alunos) {
+
+				em.persist(aluno);
+
+			}
+
+			LOGGER.info("SUCESS!");
+		} catch (RollbackException ex) {
+			LOGGER.warn("FAIL", ex);
+		} finally {
+			em.getTransaction().commit();
+			emf.close();
+		}
+	}
+
+	private static Endereco retornaEndereco() {
+
+		Endereco endereco = new Endereco();
+		endereco.setBairro("Centro");
+		endereco.setCep("58540-000");
+		endereco.setCidade("Monteiro");
+		endereco.setRua("Rua Exemplo");
+		endereco.setUf("PB");
+
+		for (int i = 1; i <= 200; i++) {
+			endereco.setNumero(i);
+			enderecos.add(endereco);
+
+		}
+
+		Collections.shuffle(enderecos);
+
+		return enderecos.get(1);
+	}
+
+	private static void retornaContato(Contato contato, Integer i) {
+
+		String e = String.valueOf(i);
+		contato.setEmail("emailnumero" + e + "@exemplo.com");
+		contato.setFacebok("Facebook " + e);
+		contato.setTwitter("Twitter " + e);
+		contatos.add(contato);
+	}
+
+	private static void retornaAluno() {
+
+		Integer matricula = 2012250219;
+
+		for (int i = 1; i <= 200; i++) {
+
+			Aluno aluno = new Aluno();
+			aluno.setEndereco(retornaEndereco());
+
+			Responsavel responsavel = new Responsavel();
+			responsavel.setNome("Nome do Responsavel");
+			responsavel.setGrauDeInstrucao("Grau de Instrucao aqui");
+			responsavel.setGrauDeParentesco("Grau de Parentesco aqui");
+			responsavel.setEndereco(retornaEndereco());
+
+			Contato contato = new Contato();
+			contato.setCelular("12345678900");
+			contato.setTelefoneResidencial("12345678900");
+			contato.setTelefoneTrabalho("12345678900");
+
+			retornaContato(contato, i);
+
+			aluno.setNome("Aluno " + String.valueOf(i));
+			aluno.setMatricula(String.valueOf(matricula + (i + 1)));
+			aluno.setResponsavel(responsavel);
+
+			contatos.get(i - 1).setAluno(aluno);
+
+			aluno.setContato(contatos.get(i - 1));
+
+			responsavel.setContato(contatos.get(i - 1));
+
+			aluno.setResponsavel(responsavel);
+
+			alunos.add(aluno);
+		}
+
+	}
+
+}
