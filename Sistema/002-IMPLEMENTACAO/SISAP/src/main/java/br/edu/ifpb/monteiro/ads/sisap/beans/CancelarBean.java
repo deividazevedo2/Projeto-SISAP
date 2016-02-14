6f48605e7263c4d7732c4c116cf614160ca032b2
widancassio <widancassio.ads@gmail.com>
2016-02-14 15:17:46 -0300

    Modifiquei alguns botoes da paginas de cadastro; adicionei a confirmação de cancelamento de cadastro;

---
+++ Sistema/002-IMPLEMENTACAO/SISAP/src/main/java/br/edu/ifpb/monteiro/ads/sisap/beans/CancelarBean.java
@@ -0,0 +1,32 @@
+package br.edu.ifpb.monteiro.ads.sisap.beans;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+@Named
+@ConversationScoped
+public class CancelarBean extends ClasseAbstrata {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6977557276403647255L;
+
+	private static final Log LOGGER = LogFactory.getLog(CancelarBean.class);
+
+	public void cancelar() {
+		addMessage("System Error", "Please try again later.");
+	}
+
+	public void addMessage(String summary, String detail) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+				summary, detail);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+}
